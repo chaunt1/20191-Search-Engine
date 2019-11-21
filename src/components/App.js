d05b2f14994aa32f6d3a1204233cfa26997c60e0
Unknown <chaulea@gmail.com>
2019-11-21 19:06:30 +0700

    Index Updated

--- src/components/App.js
+++ src/components/App.js
@@ -18,10 +18,29 @@ class App extends React.Component {
 
     render() {
         return (
-            <div className="ui container" style={{ marginTop: '10px' }}>
-                <SearchBar onSubmit={this.onSearchSubmit} />
-                Found: {this.state.images.length} images
-                <ImageList images={this.state.images} />
+            <div>
+                <div className="page header">
+                    <div className="ui container">
+                        <div className="ui text menu">
+                            <a href="/" style={{ margin: 'auto', padding: '100px' }}>
+                                <img src="/logo.png" alt="SearchHereLogo" />
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div className="ui container" style={{ marginTop: '10px' }}>
+                    <SearchBar onSubmit={this.onSearchSubmit} />
+                    Found: {this.state.images.length} images
+                    <ImageList images={this.state.images} />
+                </div>
+                <div className="footer">
+                    <div className="ui container">
+                        <div className="ui section divider"></div>
+                        <div className="ui small horizontal list">
+                            <div className="disable item">&copy; 2019 GINP14</div>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -14,7 +14,7 @@ class SearchBar extends React.Component {
             <div className="ui segment">
                 <form onSubmit={this.onFormSubmit} className="ui form">
                     <div className="field">
-                        <label>Image Search</label>
+                        <label>Input Your Search Here</label>
                         <input type="text" value={this.state.term} onChange={(e) => this.setState({term: e.target.value})} />
                     </div>
                 </form>
